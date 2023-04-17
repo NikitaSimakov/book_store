a96027fed4d108b96f10aee536f44679834074d7
NikitaSimakov <mishaaniskin@gmail.com>
2023-04-17 21:26:22 +0300

    seventh

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-// import './js/header-theme.js';
-// import './js/render-hp-default-markup.js';
+import './js/header-theme.js';
+import './js/render-hp-default-markup.js';
 import './js/support.js';
-// import './js/toggle-theme.js';
-// import './js/categories.js';
-// import './js/mobile-menu.js';
-// import './js/modalWindow.js';
+import './js/toggle-theme.js';
+import './js/categories.js';
+import './js/mobile-menu.js';
+import './js/modalWindow.js';
 
 //Приклад використання промісів з getBooksData.js
 // import { getBookData } from './js/getBooksData.js';
